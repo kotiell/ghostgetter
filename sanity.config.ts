4de30725e03149e52c5f85dcf60d50d27ece5e02
kotiell <tielljustin@gmail.com>
2023-05-27 08:00:38 -0400

    sanity working

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
 
-const config = defineConfig({});
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production';
+const config = defineConfig({
+	projectId,
+	dataset,
+	title: 'Ghost Getter',
+	apiVersion: '2026-05-26',
+	basePath: '/studio',
+	plugins: [deskTool()],
+});
 
 export default config;
